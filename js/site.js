a31d533ea48fee8300fde9fe67bec98d62e57288
Anthony <adhamlingithub@gmail.com>
2021-08-11 10:30:33 -0500

    code/script completed

--- js/site.js
+++ js/site.js
@@ -4,24 +4,33 @@
 // get the input string value
 function getValue() {
 
-    // make sure alert msg is not initially visible
+    // make sure alert msg is not initially visible and default background color is set
+    document.getElementById('alert').classList.add('alert-success');
     document.getElementById('alert').classList.add('invisible');
 
     // get the user input string
     let userStr = document.getElementById('userString').value;
 
+    // clean up user string - remove symbol, punctuation, numbers
+    userStr = userStr.toLowerCase();
+    userStr = userStr.replace(/[\p{P}$+<=>^`|~]/gu, '');
+    userStr = userStr.replace(/[^\p{L}\p{N}\s]/gu, '');
+    userStr = userStr.replace(/[^\p{L}\p{N}\p{Z}]/gu, '');
+    userStr = userStr.replace(/[^\p{L}\p{N}\s]/gu, '');
+    userStr = userStr.replace(/[0-9]/gu, '');
+    let newUserStr = userStr.split(" ").join(""); // remove space
+
     // call the reverse function
-    let reverseStr = reverseString(userStr);
+    let reverseStr = reverseString(newUserStr);
 
     // pass the reversed string to the display function
-    displayString(reverseStr);
+    displayString(reverseStr, newUserStr, userStr);
 
 }
 
 // logic function
 // reverse the string value
 function reverseString(userStr) {
-
     // declare empty array to be populated
     let reverseStr = [];
 
@@ -29,7 +38,7 @@ function reverseString(userStr) {
     for (let i = userStr.length - 1; i >= 0; i--) {
         // concatenate and populate the array with index values
         reverseStr += userStr[i];
-        
+
     }
 
     // retrun the reversed string
@@ -39,13 +48,31 @@ function reverseString(userStr) {
 
 // view function
 // display the reversed string value back to the page
-function displayString(reverseStr) {
+function displayString(reverseStr, newUserStr, userStr) {
 
-    // write to the page using a template literal
-    document.getElementById('msg').innerHTML = `${reverseStr}`;
+    if (reverseStr === newUserStr) {
+        // change alert background color
+        document.getElementById('alert').classList.remove('alert-danger');
+        document.getElementById('alert').classList.add('alert-success');
+        // write to the page using a template literal
+        document.getElementById('msg').innerHTML = `<span class="fw-bold">${userStr}</span><br><span class="fw-bold">${reverseStr}</span> is a palindrome!`;
 
-    // turn on inline alert success message
-    // (remove 'invisible' from the class listings) 
-    document.getElementById('alert').classList.remove('invisible');
+        // turn on inline alert success message
+        // (remove 'invisible' from the class listings) 
+        document.getElementById('alert').classList.remove('invisible');
+
+    } else {
+        // write to the page using a template literal
+        document.getElementById('msg').innerHTML = `<span class="fw-bold">${userStr}</span><br><span class="fw-bold">${reverseStr}</span> is not a palindrome!`;
+
+        // change alert background color
+        document.getElementById('alert').classList.remove('alert-success');
+        document.getElementById('alert').classList.add('alert-danger');
+        // turn on inline alert success message
+        // (remove 'invisible' from the class listings) 
+        document.getElementById('alert').classList.remove('invisible');
+    }
+
+
 
 }
