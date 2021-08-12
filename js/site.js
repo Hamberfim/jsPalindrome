2a01165855bee2c9c172270277c7c2888792cc77
Anthony <adhamlingithub@gmail.com>
2021-08-12 12:13:33 -0500

    a little clean up

--- js/site.js
+++ js/site.js
@@ -18,44 +18,43 @@ function getValue() {
     userStr = userStr.replace(/[^\p{L}\p{N}\p{Z}]/gu, '');
     userStr = userStr.replace(/[^\p{L}\p{N}\s]/gu, '');
     userStr = userStr.replace(/[0-9]/gu, '');
-    let newUserStr = userStr.split(" ").join(""); // remove space
+    let cleanUserStr = userStr.split(" ").join(""); // remove space
 
-    // call the reverse function
-    let reverseStr = reverseString(newUserStr);
+    // call the check function
+    let returnCheck = checkForPalindrome(cleanUserStr);
 
     // pass the reversed string to the display function
-    displayString(reverseStr, newUserStr, userStr);
+    displayString(returnCheck, cleanUserStr, userStr);
 
 }
 
 // logic function
-// reverse the string value
-function reverseString(userStr) {
+function checkForPalindrome(userStr) {
     // declare empty array to be populated
-    let reverseStr = [];
+    let revString = [];  // array
 
     // reverse string with a decrementing for loop
     for (let i = userStr.length - 1; i >= 0; i--) {
         // concatenate and populate the array with index values
-        reverseStr += userStr[i];
+        revString += userStr[i];
 
     }
 
     // retrun the reversed string
-    return reverseStr;
+    return revString;
 
 }
 
 // view function
 // display the reversed string value back to the page
-function displayString(reverseStr, newUserStr, userStr) {
+function displayString(returnCheck, cleanUserStr, userStr) {
 
-    if (reverseStr === newUserStr) {
+    if (returnCheck === cleanUserStr) {
         // change alert background color
         document.getElementById('alert').classList.remove('alert-danger');
         document.getElementById('alert').classList.add('alert-success');
         // write to the page using a template literal
-        document.getElementById('msg').innerHTML = `<span class="fw-bold">${userStr}</span><br><span class="fw-bold">${reverseStr}</span> is a palindrome!`;
+        document.getElementById('msg').innerHTML = `<span class="fw-bold">${userStr}</span><br><span class="fw-bold">${returnCheck}</span> is a palindrome!`;
 
         // turn on inline alert success message
         // (remove 'invisible' from the class listings) 
@@ -63,7 +62,7 @@ function displayString(reverseStr, newUserStr, userStr) {
 
     } else {
         // write to the page using a template literal
-        document.getElementById('msg').innerHTML = `<span class="fw-bold">${userStr}</span><br><span class="fw-bold">${reverseStr}</span> is not a palindrome!`;
+        document.getElementById('msg').innerHTML = `<span class="fw-bold">${userStr}</span><br><span class="fw-bold">${returnCheck}</span> is not a palindrome!`;
 
         // change alert background color
         document.getElementById('alert').classList.remove('alert-success');
@@ -72,7 +71,5 @@ function displayString(reverseStr, newUserStr, userStr) {
         // (remove 'invisible' from the class listings) 
         document.getElementById('alert').classList.remove('invisible');
     }
-
-
 
 }
